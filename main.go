8b95d6fa86602f141e4f102a1c88bc191dcaae9b
nozen <jacobsson.michael@gmail.com>
2019-11-06 13:29:06 +1000

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World: MICK IS WINNING"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
